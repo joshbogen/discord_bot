c28f8d8376cce58a4e7af92f2d7f0ffa5578ad0d
muzuly <59071056+muzuly@users.noreply.github.com>
2021-08-24 18:30:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -420,7 +420,7 @@ randomResponse.set(384, "Any of you mugs know where I can get a pair of concrete
 // this is the bot listener
 client.on('message', message => {
     // if the chat message starts with an bang (!)
-    if (message.content.substring(0, 1) == "!") {
+    if (message.content.substring(0, 1) == '!') {
         // parse the message and get the vbalues between the ! and the space
         var cmd = message.content.substring(1).split(' ')[0];
         // search the commands for a key that matches
